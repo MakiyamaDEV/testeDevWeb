ec7766a89cdf4fca906ef478c8c1bf3828e0fc3e
MakiyamaDEV <leandromakiyama3@gmail.com>
2025-10-17 21:27:51 -0300

    Aula17.10

---
+++ Aula17.10/js/meuscript.js
@@ -0,0 +1,14 @@
+function exibir(){
+    let paragrafo = document.getElementById("nome");
+    console.log(paragrafo);
+    paragrafo.innerHTML = "Exibindo Nome!!!"
+}
+
+function ocultar(){
+    let paragrafo = document.getElementById("nome");
+    console.log(paragrafo);
+    paragrafo.innerHTML = ""
+
+}
+
+
