07420fb0240685b0d1ac7c1a96e9b89c448e478e
MakiyamaDEV <leandromakiyama3@gmail.com>
2025-10-24 21:27:47 -0300

    Aula24.10

--- Aula17.10/js/meuscript.js
+++ Aula17.10/js/meuscript.js
@@ -5,10 +5,41 @@ function exibir(){
 }
 
 function ocultar(){
-    let paragrafo = document.getElementById("nome");
-    console.log(paragrafo);
-    paragrafo.innerHTML = ""
+   document.getElementById("nome"),innerHTML = ''
 
 }
+
+function Indo(){
+
+    document.getElementById("Aguia").src = 'Aguiaindo.webp'
+}
+
+function Voltando(){
+
+    document.getElementById("Aguia").src = 'Aguiavoltando.webp'
+}
+
+function estilizar(){
+
+    let texto = document.getElementById('texto2');
+    texto.style.fontSize = '40px';
+    texto.style = 'color: green;';
+}
+
+function esconder(){
+
+    document.getElementById('teste').style.display = 'none';
+}
+
+function ExibirParagrafo(){
+
+    document.getElementById('oculto').style.display = 'block';
+}
+
+function exibirmensagem(){
+
+    window.alert("Perigooooooo, uma ameaça foi detectada");
+}
+
 
 
